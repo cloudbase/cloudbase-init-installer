7ebb9ad5cc76b63a78470431e3bc2e4dcd6958a7
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2013-07-25 17:06:07 +0000

    Minor code cleanup

--- CloudbaseInitSetup/Actions/ConfigDlgActions.js
+++ CloudbaseInitSetup/Actions/ConfigDlgActions.js
@@ -33,9 +33,8 @@ function getNetworkAdapters() {
     for (var e = new Enumerator(networkAdapters) ; !e.atEnd() ; e.moveNext()) {
         // On XP / 2003 check the DeviceID to avoid including Miniport and other not relevant adapters
         var networkAdapter = e.item();
-        if (osVersion[0] >= 6 || networkAdapter.PNPDeviceID.indexOf("PCI") == 0) {
+        if (osVersion[0] >= 6 || networkAdapter.PNPDeviceID.indexOf("PCI") == 0)
             addComboBoxEntry(view, property, index++, networkAdapter.Name, networkAdapter.Name);
-        }
     }
 
     view.Close();
