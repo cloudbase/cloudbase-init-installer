ddd02a0537705a845bc955ec243504ae91ceba87
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-02-24 00:30:54 +0000

    Readds EC2Service in the metadata services config option

--- CloudbaseInitSetup/Actions/ConfFileActions.js
+++ CloudbaseInitSetup/Actions/ConfFileActions.js
@@ -85,7 +85,7 @@ function writeCloudbaseInitConfFileAction() {
         var cloudbaseInitConfFileUnattend = cloudbaseInitConfFolder + "cloudbase-init-unattend.conf";
 
         if (!maasMetadataUrl) {
-            config["metadata_services"] = "cloudbaseinit.metadata.services.configdrive.ConfigDriveService,cloudbaseinit.metadata.services.httpservice.HttpService,cloudbaseinit.metadata.services.maasservice.MaaSHttpService";
+            config["metadata_services"] = "cloudbaseinit.metadata.services.configdrive.ConfigDriveService,cloudbaseinit.metadata.services.httpservice.HttpService,cloudbaseinit.metadata.services.ec2service.EC2Service,cloudbaseinit.metadata.services.maasservice.MaaSHttpService";
         }
 
         config["plugins"] = "cloudbaseinit.plugins.windows.sethostname.SetHostNamePlugin";
