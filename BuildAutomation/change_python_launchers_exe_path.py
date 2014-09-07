25ccd82cffaa89fb28f7fc9144d85ab8e11822d0
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-09-07 19:18:32 +0300

    Adds script to fix Python script launchers

---
+++ BuildAutomation/change_python_launchers_exe_path.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+from pip._vendor.distlib import scripts
+
+#specs = 'nova = novaclient.shell:main'
+specs = sys.argv[1]
+
+scripts_path = os.path.join(os.path.dirname(sys.executable), 'Scripts')
+m = scripts.ScriptMaker(None, scripts_path)
+m.executable = sys.executable
+m.make(specs)
