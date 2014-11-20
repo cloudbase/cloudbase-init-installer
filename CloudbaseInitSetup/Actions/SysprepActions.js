8840eef3a372d1774846ef439e9a83bf1d3fcabe
Robert Tingirica <rtingirica@cloudbasesolutions.com>
2014-11-20 17:01:25 +0200

    Removed use for shortpath

--- CloudbaseInitSetup/Actions/SysprepActions.js
+++ CloudbaseInitSetup/Actions/SysprepActions.js
@@ -181,11 +181,6 @@ function runSysprepAction() {
 
         cmd = "\"%SystemRoot%\\System32\\Sysprep\\sysprep.exe\" /generalize /oobe";
 
-        var confFolder = Session.Property("CLOUDBASEINITCONFFOLDER");
-        // Sysprep.exe doesn't work with paths containing spaces
-        var unattendXmlPath = getShortPath(confFolder + "\\Unattend.xml")
-        cmd += " /unattend:\"" + unattendXmlPath + "\"";
-
         var shutdown = parseInt(Session.Property("SYSPREPSHUTDOWN"));
         if (!shutdown)
             cmd += " /quit";
