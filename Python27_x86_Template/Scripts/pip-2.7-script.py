e37c6cfe5692eb97e2551b74f222d0eb738b0854
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2018-08-04 18:42:23 +0300

    Remove Python27_x86_Template

--- Python27_x86_Template/Scripts/pip-2.7-script.py
+++
@@ -1,9 +0,0 @@
-#!C:\OpenStack\cloudbase-init-installer\Python27_Template\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.2.1','console_scripts','pip-2.7'
-__requires__ = 'pip==1.2.1'
-import sys
-from pkg_resources import load_entry_point
-
-sys.exit(
-   load_entry_point('pip==1.2.1', 'console_scripts', 'pip-2.7')()
-)
--- Python27_x86_Template/Scripts/wmiweb.py
+++
@@ -1,5 +0,0 @@
-#!c:\Python27\python.exe
-# EASY-INSTALL-SCRIPT: 'wmi==1.4.9','wmiweb.py'
-__requires__ = 'wmi==1.4.9'
-import pkg_resources
-pkg_resources.run_script('wmi==1.4.9', 'wmiweb.py')
