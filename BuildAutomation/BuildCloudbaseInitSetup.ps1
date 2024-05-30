1646305737e1fe229a4b39b43df3bd1b1bd8d341
Adrian Vladu <avladu@cloudbasesolutions.com>
2024-05-30 16:06:37 +0300

    python: make 3.12.3 the default build target

--- BuildAutomation/BuildCloudbaseInitSetup.ps1
+++ BuildAutomation/BuildCloudbaseInitSetup.ps1
@@ -1,6 +1,6 @@
 Param(
   [string]$platform = "x64",
-  [string]$pythonversion = "3.11_9",
+  [string]$pythonversion = "3.12_3",
   [string]$SignX509Thumbprint = $null,
   [string]$release = $null,
   # Cloudbase-Init repo details
